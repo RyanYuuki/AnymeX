213e44ffe2bed1e78c3e784ad867fd5423d79d88
Ryan _ <108048963+RyanYuuki@users.noreply.github.com>
2024-08-31 21:49:33 +0530

    - Remade Home Page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,140 +1,163 @@
 import 'dart:io';
 import 'package:aurora/components/SettingsModal.dart';
+import 'package:aurora/components/homepage/homepage_carousel.dart';
 import 'package:aurora/components/reusable_carousel.dart';
-import 'package:aurora/components/MangaExclusive/reusable_carousel.dart'
-    as ReusableCarouselManga;
+import 'package:aurora/components/MangaExclusive/reusable_carousel.dart' as MangaCarousel;
+import 'package:aurora/components/homepage/manga_homepage_carousel.dart';
 import 'package:aurora/fallbackData/anime_data.dart';
 import 'package:aurora/fallbackData/manga_data.dart';
+import 'package:aurora/pages/onboarding_screens/avatar_page.dart';
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
-class HomePage extends StatefulWidget {
+class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  @override
   Widget build(BuildContext context) {
-    var box = Hive.box('login-data');
-    final userInfo = box.get('userInfo', defaultValue: ['Guest', 'Guest', 'null']);
-    final userName = userInfo?[0] ?? 'Guest';
-    final avatarImagePath = userInfo?[2] ?? 'null';
-    final isLoggedIn = userName != 'Guest';
-    final hasAvatarImage = avatarImagePath != 'null';
+    return ValueListenableBuilder(
+      valueListenable: Hive.box('login-data').listenable(),
+      builder: (context, Box box, _) {
+        final hiveBox = Hive.box('app-data');
+        final List<dynamic>? watchingAnimeList =
+            hiveBox.get('currently-watching');
+        final List<dynamic>? readingMangaList =
+            hiveBox.get('currently-reading');
+        final userInfo =
+            box.get('userInfo', defaultValue: ['Guest', 'Guest', 'null']);
+        final userName = userInfo?[0] ?? 'Guest';
+        final avatarImagePath = userInfo?[2] ?? 'null';
+        final isLoggedIn = userName != 'Guest';
+        final hasAvatarImage = avatarImagePath != 'null';
 
-    return Scaffold(
-      backgroundColor: Theme.of(context).colorScheme.surface,
-      body: ListView(
-        children: [
-          Column(
-            children: [
-              SizedBox(
-                height: 400,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
+        return Scaffold(
+          backgroundColor: Theme.of(context).colorScheme.surface,
+          body: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: ListView(
+              children: [
+                Column(
                   children: [
-                    Padding(
-                      padding: const EdgeInsets.symmetric(
-                          horizontal: 20.0, vertical: 10),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    SizedBox(
+                      height: 400,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
                         children: [
-                          SizedBox(
-                            width: 70,
-                            height: 50,
-                            child: ClipRRect(
-                              borderRadius: BorderRadius.circular(30),
-                              child: Image.asset(
-                                'assets/images/logo_transparent.png',
-                                fit: BoxFit.cover,
-                                alignment: Alignment.center,
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              SizedBox(
+                                width: 50,
+                                height: 70,
+                                child: ClipRRect(
+                                  borderRadius: BorderRadius.circular(30),
+                                  child: Image.asset(
+                                    'assets/images/logo_transparent.png',
+                                    fit: BoxFit.cover,
+                                    alignment: Alignment.center,
+                                    color: Theme.of(context)
+                                        .colorScheme
+                                        .inverseSurface,
+                                  ),
+                                ),
                               ),
-                            ),
-                          ),
-                          GestureDetector(
-                            onTap: isLoggedIn
-                                ? () {
-                                    showModalBottomSheet(
-                                      context: context,
-                                      shape: const RoundedRectangleBorder(
-                                        borderRadius: BorderRadius.vertical(
-                                          top: Radius.circular(20),
-                                        ),
-                                      ),
-                                      builder: (context) {
-                                        return const SettingsModal();
+                              GestureDetector(
+                                onTap: isLoggedIn
+                                    ? () {
+                                        showModalBottomSheet(
+                                          context: context,
+                                          shape: const RoundedRectangleBorder(
+                                            borderRadius: BorderRadius.vertical(
+                                              top: Radius.circular(20),
+                                            ),
+                                          ),
+                                          builder: (context) {
+                                            return const SettingsModal();
+                                          },
+                                        );
+                                      }
+                                    : () {
+                                        Navigator.push(
+                                            context,
+                                            MaterialPageRoute(
+                                                builder: (context) =>
+                                                    const AvatarPage()));
                                       },
-                                    );
-                                  }
-                                : () {
-                                    Navigator.pushNamed(context, '/login-page');
-                                  },
-                            child: CircleAvatar(
-                              radius: 24,
-                              backgroundColor: Theme.of(context)
-                                  .colorScheme
-                                  .surfaceContainer,
-                              backgroundImage: hasAvatarImage
-                                  ? FileImage(File(avatarImagePath))
-                                  : null,
-                              child: hasAvatarImage
-                                  ? null
-                                  : const Icon(
-                                      Icons.person,
-                                      color: Colors.white,
-                                    ),
+                                child: CircleAvatar(
+                                  radius: 24,
+                                  backgroundColor: Theme.of(context)
+                                      .colorScheme
+                                      .surfaceContainer,
+                                  backgroundImage: hasAvatarImage
+                                      ? FileImage(File(avatarImagePath))
+                                      : null,
+                                  child: hasAvatarImage
+                                      ? null
+                                      : const Icon(
+                                          Icons.person,
+                                          color: Colors.white,
+                                        ),
+                                ),
+                              ),
+                            ],
+                          ),
+                          const SizedBox(height: 60),
+                          const Text(
+                            'What are you looking for?',
+                            style: TextStyle(
+                                fontSize: 40, fontFamily: 'Poppins-Bold'),
+                            textAlign: TextAlign.center,
+                          ),
+                          const SizedBox(height: 10),
+                          Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 20),
+                            child: Text(
+                              'Find your favorite anime or manga, manhwa or whatever you like!',
+                              style: TextStyle(
+                                color: Theme.of(context)
+                                    .colorScheme
+                                    .inverseSurface
+                                    .withOpacity(0.8),
+                              ),
+                              textAlign: TextAlign.center,
                             ),
                           ),
                         ],
                       ),
                     ),
-                    const SizedBox(height: 70),
-                    const Text(
-                      'What are you looking for?',
-                      style: TextStyle(fontSize: 40, fontFamily: 'Poppins-Bold'),
-                      textAlign: TextAlign.center,
-                    ),
-                    const SizedBox(height: 10),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 20),
-                      child: Text(
-                        'Find your favorite anime or manga, manhwa or whatever you like!',
-                        style: TextStyle(
-                          color: Theme.of(context)
-                              .colorScheme
-                              .inverseSurface
-                              .withOpacity(0.8),
+                    Column(
+                      children: [
+                        HomepageCarousel(
+                          title: 'Currently Watching',
+                          carouselData: watchingAnimeList,
+                          tag: 'home-page',
                         ),
-                        textAlign: TextAlign.center,
-                      ),
+                        MangaHomepageCarousel(
+                          title: 'Currently Reading',
+                          carouselData: readingMangaList,
+                          tag: 'home-page',
+                        ),
+                        ReusableCarousel(
+                          title: 'Recommended',
+                          carouselData: animeData['topAiringAnimes'],
+                          tag: 'home-page-recommended',
+                          secondary: false,
+                        ),
+                        MangaCarousel.ReusableCarousel(
+                          title: 'Recommended',
+                          carouselData: mangaData['mangaList'],
+                          tag: 'home-page-recommended',
+                          secondary: false,
+                        ),
+                      ],
                     ),
                   ],
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 10),
-                child: Column(
-                  children: [
-                    ReusableCarousel(
-                      title: 'Top Airing',
-                      carouselData: animeData['topAiringAnimes'],
-                      tag: 'home-page',
-                    ),
-                    ReusableCarouselManga.ReusableCarousel(
-                      title: 'Top',
-                      carouselData: moreMangaData['mangaList'],
-                      tag: 'home-page',
-                    ),
-                  ],
-                ),
-              ),
-            ],
+              ],
+            ),
           ),
-        ],
-      ),
+        );
+      },
     );
   }
 }
