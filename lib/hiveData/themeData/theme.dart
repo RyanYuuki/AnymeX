0d8c370c30684cf70d2daaf8a51fbb9a43ea977d
Ryan _ <108048963+RyanYuuki@users.noreply.github.com>
2024-10-26 14:05:09 +0530

    - Organizing Files (1/5)

---
+++ lib/hiveData/themeData/theme.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+
+const Color seedColor = Colors.indigo;
+
+ThemeData lightMode = ThemeData(
+  useMaterial3: true,
+  fontFamily: 'Poppins',
+  brightness: Brightness.light,
+  colorScheme: ColorScheme.fromSeed(
+    seedColor: seedColor,
+    brightness: Brightness.light,
+  ),
+  textTheme: const TextTheme(
+    bodyLarge: TextStyle(color: Colors.black),
+    bodyMedium: TextStyle(color: Colors.black),
+    titleLarge: TextStyle(
+        color: Colors.black, fontWeight: FontWeight.bold, fontSize: 24),
+    bodySmall: TextStyle(color: Colors.white, fontSize: 12),
+  ),
+  inputDecorationTheme: InputDecorationTheme(
+    filled: true,
+    fillColor: Colors.grey.shade300,
+    hintStyle: TextStyle(color: Colors.grey.shade600),
+    prefixIconColor: Colors.grey.shade700,
+    suffixIconColor: Colors.grey.shade700,
+    border: OutlineInputBorder(
+      borderRadius: BorderRadius.circular(12),
+      borderSide: BorderSide.none,
+    ),
+  ),
+  buttonTheme: const ButtonThemeData(
+    buttonColor: seedColor,
+    textTheme: ButtonTextTheme.primary,
+  ),
+  elevatedButtonTheme: ElevatedButtonThemeData(
+    style: ElevatedButton.styleFrom(
+      foregroundColor: Colors.white,
+      backgroundColor: seedColor,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(5),
+      ),
+    ),
+  ),
+  iconTheme: const IconThemeData(
+    color: Colors.black, 
+    size: 24, 
+  ),
+  floatingActionButtonTheme: const FloatingActionButtonThemeData(
+    backgroundColor: seedColor,
+    foregroundColor: Colors.white,
+  ),
+);
+
+ThemeData darkMode = ThemeData(
+  useMaterial3: true,
+  fontFamily: 'Poppins',
+  brightness: Brightness.dark,
+  colorScheme: ColorScheme.fromSeed(
+    seedColor: seedColor,
+    brightness: Brightness.dark,
+  ),
+  textTheme: const TextTheme(
+    bodyLarge: TextStyle(color: Colors.white),
+    bodyMedium: TextStyle(color: Colors.white),
+    titleLarge: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+    bodySmall: TextStyle(color: Colors.grey, fontSize: 12), // Updated to a lighter color
+  ),
+  inputDecorationTheme: InputDecorationTheme(
+    filled: true,
+    fillColor: Colors.grey.shade900,
+    hintStyle: TextStyle(color: Colors.grey.shade400),
+    prefixIconColor: Colors.grey.shade500,
+    suffixIconColor: Colors.grey.shade500,
+    border: OutlineInputBorder(
+      borderRadius: BorderRadius.circular(12),
+      borderSide: BorderSide.none,
+    ),
+  ),
+  buttonTheme: const ButtonThemeData(
+    buttonColor: seedColor,
+    textTheme: ButtonTextTheme.primary,
+  ),
+  elevatedButtonTheme: ElevatedButtonThemeData(
+    style: ElevatedButton.styleFrom(
+      foregroundColor: Colors.white,
+      backgroundColor: seedColor,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(5),
+      ),
+    ),
+  ),
+  iconTheme: const IconThemeData(
+    color: Colors.white, 
+    size: 24, 
+  ),
+  floatingActionButtonTheme: const FloatingActionButtonThemeData(
+    backgroundColor: seedColor,
+    foregroundColor: Colors.white,
+  ),
+);
