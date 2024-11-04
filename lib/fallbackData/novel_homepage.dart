3ff0ad2aee0ba40aef335c9c4a375bd1aa846ef4
Ryan _ <108048963+RyanYuuki@users.noreply.github.com>
2024-11-05 01:05:51 +0530

    - added fallback data for novel for instant load

---
+++ lib/fallbackData/novel_homepage.dart
@@ -0,0 +1,402 @@
+const novelFallbackData = [
+  {
+    "id": "https://wuxia.click/novel/alchemy-emperor-of-the-divine-dao",
+    "title": "Alchemy Emperor of the Divine Dao",
+    "image":
+        "https://imaginary.wuxiaworld.eu/resize?url=https%3A%2F%2Fcdn.wuxiaworld.eu%2Foriginal%2FAlchemy-Emperor-of-the-Divine-Dao-.jpg&type=webp&width=300&quality=100",
+    "rating": "6.46",
+    "rank": "2",
+    "views": "9.96M",
+    "chapters": "3199"
+  },
+  {
+    "id": "https://wuxia.click/novel/super-god-gene",
+    "title": "Super God Gene",
+    "image":
+        "https://imaginary.wuxiaworld.eu/resize?url=https%3A%2F%2Fcdn.wuxiaworld.eu%2Foriginal%2Fsuper-god-gene.webp&type=webp&width=300&quality=100",
+    "rating": "6.1",
+    "rank": "7",
+    "views": "4.57M",
+    "chapters": "3462"
+  },
+  {
+    "id": "https://wuxia.click/novel/unrivaled-medicine-god",
+    "title": "Unrivaled Medicine God",
+    "image":
+        "https://imaginary.wuxiaworld.eu/resize?url=https%3A%2F%2Fcdn.wuxiaworld.eu%2Foriginal%2FUnrivaled-Medicine-God.jpg&type=webp&width=300&quality=100",
+    "rating": "5.68",
+    "rank": "9",
+    "views": "4.03M",
+    "chapters": "3125"
+  },
+  {
+    "id": "https://wuxia.click/novel/the-legend-of-futian",
+    "title": "The Legend of Futian",
+    "image":
+        "https://imaginary.wuxiaworld.eu/resize?url=https%3A%2F%2Fcdn.wuxiaworld.eu%2Foriginal%2FThe-Legend-of-Futian-.jpg&type=webp&width=300&quality=100",
+    "rating": "7.02",
+    "rank": "14",
+    "views": "2.96M",
+    "chapters": "2970"
+  },
+  {
+    "id": "https://wuxia.click/novel/legend-of-swordsman",
+    "title": "Legend of Swordsman",
+    "image":
+        "https://imaginary.wuxiaworld.eu/resize?url=https%3A%2F%2Fcdn.wuxiaworld.eu%2Foriginal%2FLegend-of-Swordsman.jpg&type=webp&width=300&quality=100",
+    "rating": "7.0",
+    "rank": "15",
+    "views": "2.86M",
+    "chapters": "6205"
+  },
+  {
+    "id": "https://wuxia.click/novel/emperors-domination",
+    "title": "Emperor’s Domination",
+    "image":
+        "https://imaginary.wuxiaworld.eu/resize?url=https%3A%2F%2Fcdn.wuxiaworld.eu%2Foriginal%2Fliqiye.jpg&type=webp&width=300&quality=100",
+    "rating": "7.24",
+    "rank": "16",
+    "views": "2.63M",
+    "chapters": "5451"
+  },
+  {
+    "id": "https://wuxia.click/novel/versatile-mage",
+    "title": "Versatile Mage",
+    "image":
+        "https://imaginary.wuxiaworld.eu/resize?url=https%3A%2F%2Fcdn.wuxiaworld.eu%2Foriginal%2FVersatile-Mage.jpg&type=webp&width=300&quality=100",
+    "rating": "6.68",
+    "rank": "18",
+    "views": "2.45M",
+    "chapters": "3128"
+  },
+  {
+    "id": "https://wuxia.click/novel/nine-star-hegemon-body-art",
+    "title": "Nine Star Hegemon Body Art",
+    "image":
+        "https://imaginary.wuxiaworld.eu/resize?url=https%3A%2F%2Fcdn.wuxiaworld.eu%2Foriginal%2FNine-Star-Hegemon-Body-Art.jpg&type=webp&width=300&quality=100",
+    "rating": "6.6",
+    "rank": "19",
+    "views": "2.33M",
+    "chapters": "3699"
+  },
+  {
+    "id": "https://wuxia.click/novel/first-immortal-of-the-sword",
+    "title": "First Immortal of the Sword",
+    "image":
+        "https://imaginary.wuxiaworld.eu/resize?url=https%3A%2F%2Fcdn.wuxiaworld.eu%2Foriginal%2FFirst-Immortal-of-the-Sword.jpg&type=webp&width=300&quality=100",
+    "rating": "7.0",
+    "rank": "21",
+    "views": "2.04M",
+    "chapters": "905"
+  },
+  {
+    "id": "https://wuxia.click/novel/war-sovereign-soaring-the-heavens",
+    "title": "War Sovereign Soaring The Heavens",
+    "image":
+        "https://imaginary.wuxiaworld.eu/resize?url=https%3A%2F%2Fcdn.wuxiaworld.eu%2Foriginal%2FWar-Sovereign-Soaring-The-Heavens.jpg&type=webp&width=300&quality=100",
+    "rating": "5.1",
+    "rank": "22",
+    "views": "2M",
+    "chapters": "2146"
+  },
+  {
+    "id": "https://wuxia.click/novel/omniscient-readers-viewpoint",
+    "title": "Omniscient Reader’s Viewpoint",
+    "image":
+        "https://imaginary.wuxiaworld.eu/resize?url=https%3A%2F%2Fcdn.wuxiaworld.eu%2Foriginal%2FOmniscient-Readers-Viewpoint.png&type=webp&width=300&quality=100",
+    "rating": "9.18",
+    "rank": "3",
+    "views": "9.34M",
+    "chapters": "551"
+  },
+  {
+    "id": "https://wuxia.click/novel/reincarnation-of-the-strongest-sword-god",
+    "title": "Reincarnation Of The Strongest Sword God",
+    "image":
+        "https://imaginary.wuxiaworld.eu/resize?url=https%3A%2F%2Fcdn.wuxiaworld.eu%2Foriginal%2FReincarnation-Of-The-Strongest-Sword-God.jpg&type=webp&width=300&quality=100",
+    "rating": "6.48",
+    "rank": "4",
+    "views": "8.14M",
+    "chapters": "3718"
+  },
+  {
+    "id": "https://wuxia.click/novel/the-problematic-prince",
+    "title": "The Problematic Prince",
+    "image":
+        "https://imaginary.wuxiaworld.eu/resize?url=https%3A%2F%2Fcdn.wuxiaworld.eu%2Foriginal%2FThe-Problematic-Prince.jpg&type=webp&width=300&quality=100",
+    "rating": "8.66",
+    "rank": "6",
+    "views": "4.98M",
+    "chapters": "154"
+  },
+  {
+    "id": "https://wuxia.click/novel/academys-undercover-professor",
+    "title": "Academy’s Undercover Professor",
+    "image":
+        "https://imaginary.wuxiaworld.eu/resize?url=https%3A%2F%2Fcdn.wuxiaworld.eu%2Foriginal%2Facademys-undercover-professor_cover_uLlwdeA.jpg&type=webp&width=300&quality=100",
+    "rating": "7.96",
+    "rank": "11",
+    "views": "3.76M",
+    "chapters": "352"
+  },
+  {
+    "id": "https://wuxia.click/novel/qiang-jin-jiu",
+    "title": "Qiang Jin Jiu",
+    "image":
+        "https://imaginary.wuxiaworld.eu/resize?url=https%3A%2F%2Fcdn.wuxiaworld.eu%2Foriginal%2FQiang-Jin-Jiu.jpg&type=webp&width=300&quality=100",
+    "rating": "9.57",
+    "rank": "5",
+    "views": "7.48M",
+    "chapters": "481"
+  },
+  {
+    "id": "https://wuxia.click/novel/three-heroic-ghosts",
+    "title": "Three Heroic Ghosts",
+    "image":
+        "https://imaginary.wuxiaworld.eu/resize?url=https%3A%2F%2Fcdn.wuxiaworld.eu%2Foriginal%2Fthree-heroic-ghosts.jpg&type=webp&width=300&quality=100",
+    "rating": "9.75",
+    "rank": "8",
+    "views": "4.53M",
+    "chapters": "471"
+  },
+  {
+    "id": "https://wuxia.click/novel/spirit-hunters",
+    "title": "Spirit Hunters",
+    "image":
+        "https://imaginary.wuxiaworld.eu/resize?url=https%3A%2F%2Fcdn.wuxiaworld.eu%2Foriginal%2Fspirit-hunters.jpg&type=webp&width=300&quality=100",
+    "rating": "9.47",
+    "rank": "1",
+    "views": "9.99M",
+    "chapters": "485"
+  },
+  {
+    "id": "https://wuxia.click/novel/my-dreams-since-the-8th-grade",
+    "title": "My Dreams Since The 8th Grade",
+    "image":
+        "https://imaginary.wuxiaworld.eu/resize?url=https%3A%2F%2Fcdn.wuxiaworld.eu%2Foriginal%2Fmy-dreams-since-the-8th-grade.jpg&type=webp&width=300&quality=100",
+    "rating": "9.5",
+    "rank": "13",
+    "views": "3.51M",
+    "chapters": "209"
+  },
+  {
+    "id": "https://wuxia.click/novel/cultivation-online",
+    "title": "Cultivation Online",
+    "image":
+        "https://imaginary.wuxiaworld.eu/resize?url=https%3A%2F%2Fcdn.wuxiaworld.eu%2Foriginal%2Fcultivation-online.jpg&type=webp&width=300&quality=100",
+    "rating": "9.5",
+    "rank": "20",
+    "views": "2.02M",
+    "chapters": "261"
+  },
+  {
+    "id": "https://wuxia.click/novel/a-monster-is-she",
+    "title": "A Monster Is She?",
+    "image":
+        "https://imaginary.wuxiaworld.eu/resize?url=https%3A%2F%2Fcdn.wuxiaworld.eu%2Foriginal%2Fa-monster-is-she.jpg&type=webp&width=300&quality=100",
+    "rating": "9.3",
+    "rank": "23",
+    "views": "1.95M",
+    "chapters": "241"
+  },
+  {
+    "id": "https://wuxia.click/novel/hungry-ghost-comes-back",
+    "title": "Hungry Ghost Comes Back",
+    "image":
+        "https://imaginary.wuxiaworld.eu/resize?url=https%3A%2F%2Fcdn.wuxiaworld.eu%2Foriginal%2Fhungry-ghost-comes-back.jpg&type=webp&width=300&quality=100",
+    "rating": "9.8",
+    "rank": "24",
+    "views": "1.86M",
+    "chapters": "195"
+  },
+  {
+    "id": "https://wuxia.click/novel/summoning-the-nuclear-fusion-demon",
+    "title": "Summoning the Nuclear Fusion Demon",
+    "image":
+        "https://imaginary.wuxiaworld.eu/resize?url=https%3A%2F%2Fcdn.wuxiaworld.eu%2Foriginal%2Fsummoning-the-nuclear-fusion-demon.jpg&type=webp&width=300&quality=100",
+    "rating": "9.99",
+    "rank": "25",
+    "views": "1.76M",
+    "chapters": "55"
+  },
+  {
+    "id": "https://wuxia.click/novel/crazy-holidays",
+    "title": "Crazy Holidays",
+    "image":
+        "https://imaginary.wuxiaworld.eu/resize?url=https%3A%2F%2Fcdn.wuxiaworld.eu%2Foriginal%2Fcrazy-holidays.jpg&type=webp&width=300&quality=100",
+    "rating": "9.2",
+    "rank": "12",
+    "views": "2.13M",
+    "chapters": "118"
+  },
+  {
+    "id": "https://wuxia.click/novel/our-secret",
+    "title": "Our Secret",
+    "image":
+        "https://imaginary.wuxiaworld.eu/resize?url=https%3A%2F%2Fcdn.wuxiaworld.eu%2Foriginal%2Four-secret.jpg&type=webp&width=300&quality=100",
+    "rating": "9.1",
+    "rank": "10",
+    "views": "1.72M",
+    "chapters": "157"
+  },
+  {
+    "id": "https://wuxia.click/novel/reborn-as-a-mom",
+    "title": "Reborn As A Mom",
+    "image":
+        "https://imaginary.wuxiaworld.eu/resize?url=https%3A%2F%2Fcdn.wuxiaworld.eu%2Foriginal%2Freborn-as-a-mom.jpg&type=webp&width=300&quality=100",
+    "rating": "9.5",
+    "rank": "30",
+    "views": "1.61M",
+    "chapters": "175"
+  },
+  {
+    "id": "https://wuxia.click/novel/untouchable",
+    "title": "Untouchable",
+    "image":
+        "https://imaginary.wuxiaworld.eu/resize?url=https%3A%2F%2Fcdn.wuxiaworld.eu%2Foriginal%2Funtouchable.jpg&type=webp&width=300&quality=100",
+    "rating": "8.9",
+    "rank": "26",
+    "views": "1.54M",
+    "chapters": "122"
+  },
+  {
+    "id": "https://wuxia.click/novel/living-as-a-ninja",
+    "title": "Living As A Ninja",
+    "image":
+        "https://imaginary.wuxiaworld.eu/resize?url=https%3A%2F%2Fcdn.wuxiaworld.eu%2Foriginal%2Fliving-as-a-ninja.jpg&type=webp&width=300&quality=100",
+    "rating": "9.7",
+    "rank": "27",
+    "views": "1.37M",
+    "chapters": "234"
+  },
+  {
+    "id": "https://wuxia.click/novel/my-mom-is-an-elf",
+    "title": "My Mom Is An Elf",
+    "image":
+        "https://imaginary.wuxiaworld.eu/resize?url=https%3A%2F%2Fcdn.wuxiaworld.eu%2Foriginal%2Fmy-mom-is-an-elf.jpg&type=webp&width=300&quality=100",
+    "rating": "9.75",
+    "rank": "28",
+    "views": "1.27M",
+    "chapters": "152"
+  },
+  {
+    "id": "https://wuxia.click/novel/the-mad-scientist",
+    "title": "The Mad Scientist",
+    "image":
+        "https://imaginary.wuxiaworld.eu/resize?url=https%3A%2F%2Fcdn.wuxiaworld.eu%2Foriginal%2Fthe-mad-scientist.jpg&type=webp&width=300&quality=100",
+    "rating": "9.1",
+    "rank": "29",
+    "views": "1.04M",
+    "chapters": "133"
+  },
+  {
+    "id": "https://wuxia.click/novel/wholesome-dads",
+    "title": "Wholesome Dads",
+    "image":
+        "https://imaginary.wuxiaworld.eu/resize?url=https%3A%2F%2Fcdn.wuxiaworld.eu%2Foriginal%2Fwholesome-dads.jpg&type=webp&width=300&quality=100",
+    "rating": "9.45",
+    "rank": "32",
+    "views": "968K",
+    "chapters": "98"
+  },
+  {
+    "id": "https://wuxia.click/novel/hooligan",
+    "title": "Hooligan",
+    "image":
+        "https://imaginary.wuxiaworld.eu/resize?url=https%3A%2F%2Fcdn.wuxiaworld.eu%2Foriginal%2Fhooligan.jpg&type=webp&width=300&quality=100",
+    "rating": "9.0",
+    "rank": "37",
+    "views": "931K",
+    "chapters": "128"
+  },
+  {
+    "id": "https://wuxia.click/novel/fiancee-is-a-music-master",
+    "title": "Fiancee Is A Music Master",
+    "image":
+        "https://imaginary.wuxiaworld.eu/resize?url=https%3A%2F%2Fcdn.wuxiaworld.eu%2Foriginal%2Ffiancee-is-a-music-master.jpg&type=webp&width=300&quality=100",
+    "rating": "9.2",
+    "rank": "35",
+    "views": "745K",
+    "chapters": "119"
+  },
+  {
+    "id": "https://wuxia.click/novel/fated-to-be-a-hero",
+    "title": "Fated to Be a Hero",
+    "image":
+        "https://imaginary.wuxiaworld.eu/resize?url=https%3A%2F%2Fcdn.wuxiaworld.eu%2Foriginal%2Ffated-to-be-a-hero.jpg&type=webp&width=300&quality=100",
+    "rating": "9.8",
+    "rank": "38",
+    "views": "728K",
+    "chapters": "82"
+  },
+  {
+    "id": "https://wuxia.click/novel/school-of-magic",
+    "title": "School of Magic",
+    "image":
+        "https://imaginary.wuxiaworld.eu/resize?url=https%3A%2F%2Fcdn.wuxiaworld.eu%2Foriginal%2Fschool-of-magic.jpg&type=webp&width=300&quality=100",
+    "rating": "9.4",
+    "rank": "39",
+    "views": "726K",
+    "chapters": "150"
+  },
+  {
+    "id": "https://wuxia.click/novel/magic-essence",
+    "title": "Magic Essence",
+    "image":
+        "https://imaginary.wuxiaworld.eu/resize?url=https%3A%2F%2Fcdn.wuxiaworld.eu%2Foriginal%2Fmagic-essence.jpg&type=webp&width=300&quality=100",
+    "rating": "9.1",
+    "rank": "40",
+    "views": "615K",
+    "chapters": "120"
+  },
+  {
+    "id": "https://wuxia.click/novel/adam-the-mightiest",
+    "title": "Adam the Mightiest",
+    "image":
+        "https://imaginary.wuxiaworld.eu/resize?url=https%3A%2F%2Fcdn.wuxiaworld.eu%2Foriginal%2Fadam-the-mightiest.jpg&type=webp&width=300&quality=100",
+    "rating": "9.7",
+    "rank": "41",
+    "views": "615K",
+    "chapters": "157"
+  },
+  {
+    "id": "https://wuxia.click/novel/return-of-the-hero",
+    "title": "Return of the Hero",
+    "image":
+        "https://imaginary.wuxiaworld.eu/resize?url=https%3A%2F%2Fcdn.wuxiaworld.eu%2Foriginal%2Freturn-of-the-hero.jpg&type=webp&width=300&quality=100",
+    "rating": "9.1",
+    "rank": "42",
+    "views": "614K",
+    "chapters": "133"
+  },
+  {
+    "id": "https://wuxia.click/novel/gunpowder-vigilante",
+    "title": "Gunpowder Vigilante",
+    "image":
+        "https://imaginary.wuxiaworld.eu/resize?url=https%3A%2F%2Fcdn.wuxiaworld.eu%2Foriginal%2Fgunpowder-vigilante.jpg&type=webp&width=300&quality=100",
+    "rating": "9.4",
+    "rank": "43",
+    "views": "574K",
+    "chapters": "150"
+  },
+  {
+    "id": "https://wuxia.click/novel/undead-hero",
+    "title": "Undead Hero",
+    "image":
+        "https://imaginary.wuxiaworld.eu/resize?url=https%3A%2F%2Fcdn.wuxiaworld.eu%2Foriginal%2Fundead-hero.jpg&type=webp&width=300&quality=100",
+    "rating": "9.6",
+    "rank": "44",
+    "views": "503K",
+    "chapters": "120"
+  },
+  {
+    "id": "https://wuxia.click/novel/the-maiden-and-the-demon",
+    "title": "The Maiden and the Demon",
+    "image":
+        "https://imaginary.wuxiaworld.eu/resize?url=https%3A%2F%2Fcdn.wuxiaworld.eu%2Foriginal%2Fthe-maiden-and-the-demon.jpg&type=webp&width=300&quality=100",
+    "rating": "9.2",
+    "rank": "45",
+    "views": "467K",
+    "chapters": "50"
+  }
+];
