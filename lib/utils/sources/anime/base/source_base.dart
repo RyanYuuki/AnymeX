b69494d6c9b0253d6b31410b08dc6af02d5bf196
Ryan _ <108048963+RyanYuuki@users.noreply.github.com>
2024-11-12 01:51:56 +0530

    - init source base for anime base source

---
+++ lib/utils/sources/anime/base/source_base.dart
@@ -0,0 +1,10 @@
+import 'package:aurora/utils/sources/anime/extensions/aniwatch/aniwatch.dart';
+
+abstract class SourceBase {
+  String get sourceName;
+  bool get isMulti;
+  Future<dynamic> scrapeSearchResults(String query) async {}
+  Future<dynamic> scrapeEpisodes(String url) async {}
+  Future<dynamic> scrapeEpisodesSrcs(String episodeId,
+      {AnimeServers? server, String? category}) async {}
+}
