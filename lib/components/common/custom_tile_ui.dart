df4cc14b7d84efc36b4e895c18460f1883bf8499
Ryan _ <108048963+RyanYuuki@users.noreply.github.com>
2024-11-12 01:43:08 +0530

    - New Setting UI

---
+++ lib/components/common/custom_tile_ui.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+class CustomTileUi extends StatelessWidget {
+  final IconData icon;
+  final String title;
+  final String description;
+
+  const CustomTileUi({
+    super.key,
+    required this.icon,
+    required this.title,
+    required this.description,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {},
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+        child: Row(
+          children: [
+            Icon(icon, size: 30, color: Theme.of(context).colorScheme.primary),
+            const SizedBox(width: 20),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    title,
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w600,
+                      color: Theme.of(context).colorScheme.onSurface,
+                    ),
+                  ),
+                  const SizedBox(height: 5),
+                  Text(
+                    description,
+                    style: TextStyle(
+                      fontSize: 14,
+                      color: Theme.of(context)
+                          .colorScheme
+                          .onSurface
+                          .withOpacity(0.6),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
