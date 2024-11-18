89134705268cfdb0ddd2c4c8c6751bb0e4e5a204
Ryan _ <108048963+RyanYuuki@users.noreply.github.com>
2024-11-19 00:34:28 +0530

    - impl unified handler to share info across all sources

---
+++ lib/utils/sources/unified_handler.dart
@@ -0,0 +1,24 @@
+import 'package:aurora/utils/sources/anime/handler/sources_handler.dart';
+import 'package:aurora/utils/sources/manga/handlers/manga_sources_handler.dart';
+import 'package:aurora/utils/sources/novel/handler/novel_sources_handler.dart';
+import 'package:flutter/material.dart';
+
+class UnifiedSourcesHandler extends ChangeNotifier {
+  SourcesHandler animeHandler = SourcesHandler();
+  MangaSourceHandler mangaSourceHandler = MangaSourceHandler();
+  NovelSourcesHandler novelSourcesHandler = NovelSourcesHandler();
+
+  UnifiedSourcesHandler();
+
+  SourcesHandler getAnimeInstance() {
+    return animeHandler;
+  }
+
+  MangaSourceHandler getMangaInstance() {
+    return mangaSourceHandler;
+  }
+
+  NovelSourcesHandler getNovelInstance() {
+    return novelSourcesHandler;
+  }
+}
