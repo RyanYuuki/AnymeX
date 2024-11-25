398cf6870529b4294d141209c4011228ff79c623
Ryan _ <108048963+RyanYuuki@users.noreply.github.com>
2024-11-26 00:37:30 +0530

    feat: implemented a helper for desktop layout

---
+++ lib/components/platform_builder.dart
@@ -0,0 +1,25 @@
+import 'package:aurora/utils/dimensions.dart';
+import 'package:flutter/material.dart';
+
+class PlatformBuilder extends StatefulWidget {
+  final Widget androidBuilder;
+  final Widget desktopBuilder;
+  const PlatformBuilder(
+      {super.key, required this.androidBuilder, required this.desktopBuilder});
+
+  @override
+  State<PlatformBuilder> createState() => _PlatformBuilderState();
+}
+
+class _PlatformBuilderState extends State<PlatformBuilder> {
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(builder: (context, constraints) {
+      if (constraints.maxWidth > maxMobileWidth) {
+        return widget.desktopBuilder;
+      } else {
+        return widget.androidBuilder;
+      }
+    });
+  }
+}
