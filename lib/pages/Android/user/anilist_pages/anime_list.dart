e7395453a504615c9c0762e52bebbe1ab87becb7
Ryan _ <108048963+RyanYuuki@users.noreply.github.com>
2024-11-29 18:29:47 +0530

    style: made the anime list page responsive

--- lib/pages/Android/user/anilist_pages/anime_list.dart
+++ lib/pages/Android/user/anilist_pages/anime_list.dart
@@ -1,5 +1,6 @@
 import 'dart:math';
 import 'package:aurora/auth/auth_provider.dart';
+import 'package:aurora/components/platform_builder.dart';
 import 'package:aurora/pages/Android/Anime/details_page.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
@@ -32,7 +33,9 @@ class AnimeList extends StatelessWidget {
       length: tabs.length,
       child: Scaffold(
         appBar: AppBar(
-          automaticallyImplyLeading: false,
+          leading: IconButton(
+              onPressed: () => Navigator.pop(context),
+              icon: const Icon(Icons.arrow_back_ios_new)),
           title: Text("$userName's Anime List",
               style: TextStyle(
                   fontSize: 16, color: Theme.of(context).colorScheme.primary)),
@@ -70,6 +73,10 @@ class AnimeListContent extends StatelessWidget {
   const AnimeListContent(
       {super.key, required this.tabType, required this.animeData});
 
+  int getResponsiveCrossAxisCount(double screenWidth, {int itemWidth = 150}) {
+    return (screenWidth / itemWidth).floor().clamp(1, 10);
+  }
+
   @override
   Widget build(BuildContext context) {
     if (animeData == null) {
@@ -84,132 +91,260 @@ class AnimeListContent extends StatelessWidget {
 
     return Padding(
       padding: const EdgeInsets.all(8.0),
-      child: GridView.builder(
-        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 3, mainAxisExtent: 260, crossAxisSpacing: 10),
-        itemCount: filteredAnimeList.length,
-        itemBuilder: (context, index) {
-          final item = filteredAnimeList[index]['media'];
-          final tag = '${Random().nextInt(100000)}$index';
-          final posterUrl = item?['coverImage']?['large'] ??
-              'https://s4.anilist.co/file/anilistcdn/media/anime/cover/large/bx16498-73IhOXpJZiMF.jpg';
-          return GestureDetector(
-            onTap: () {
-              Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => DetailsPage(
-                            id: item['id'],
-                            tag: tag,
-                            posterUrl: posterUrl,
-                          )));
-            },
-            child: Column(
-              children: [
-                Stack(children: [
-                  Hero(
-                    tag: tag,
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(18),
-                      child: CachedNetworkImage(
-                        height: 170,
-                        fit: BoxFit.cover,
-                        imageUrl: posterUrl,
-                        placeholder: (context, url) =>
-                            const Center(child: CircularProgressIndicator()),
-                        errorWidget: (context, url, error) =>
-                            const Icon(Icons.error),
-                      ),
-                    ),
-                  ),
-                  Positioned(
-                      bottom: 0,
-                      right: 0,
-                      child: Container(
-                        padding: const EdgeInsets.symmetric(
-                            vertical: 4, horizontal: 8),
-                        decoration: BoxDecoration(
-                            color:
-                                Theme.of(context).colorScheme.surfaceContainer,
-                            borderRadius: const BorderRadius.only(
-                                topLeft: Radius.circular(12),
-                                bottomRight: Radius.circular(16))),
-                        child: Row(
-                          children: [
-                            Icon(
-                              Iconsax.star5,
-                              size: 11,
-                              color: Theme.of(context).colorScheme.primary,
-                            ),
-                            const SizedBox(width: 2),
-                            Text(
-                              (item?['averageScore'] / 10)?.toString() ?? '0.0',
-                              style: TextStyle(
-                                  fontSize: 11,
-                                  color: Theme.of(context)
-                                              .colorScheme
-                                              .inverseSurface ==
-                                          Theme.of(context)
-                                              .colorScheme
-                                              .onPrimaryFixedVariant
-                                      ? Colors.black
-                                      : Theme.of(context)
-                                                  .colorScheme
-                                                  .onPrimaryFixedVariant ==
-                                              const Color(0xffe2e2e2)
-                                          ? Colors.black
-                                          : Colors.white),
-                            ),
-                          ],
-                        ),
-                      )),
-                ]),
-                const SizedBox(height: 7),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      item?['title']?['english'] ??
-                          item?['title']?['romaji'] ??
-                          '?',
-                      maxLines: 2,
-                      style: const TextStyle(fontSize: 12),
-                    ),
-                    const SizedBox(height: 2),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          filteredAnimeList[index]?['progress']?.toString() ??
-                              '?',
-                          style: TextStyle(
-                              color: Theme.of(context)
-                                  .colorScheme
-                                  .onPrimaryContainer),
-                        ),
-                        Text(' | ',
-                            style: TextStyle(
-                                color: Theme.of(context)
-                                    .colorScheme
-                                    .inverseSurface
-                                    .withOpacity(0.5))),
-                        Text(
-                          item['episodes']?.toString() ?? '?',
-                          style: TextStyle(
-                              color: Theme.of(context)
-                                  .colorScheme
-                                  .inverseSurface
-                                  .withOpacity(0.5)),
-                        ),
-                      ],
-                    ),
-                  ],
-                )
-              ],
+      child: PlatformBuilder(
+        androidBuilder: GridView.builder(
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 3, mainAxisExtent: 260, crossAxisSpacing: 10),
+          itemCount: filteredAnimeList.length,
+          itemBuilder: (context, index) {
+            final item = filteredAnimeList[index]['media'];
+            final tag = '${Random().nextInt(100000)}$index';
+            final posterUrl = item?['coverImage']?['large'] ??
+                'https://s4.anilist.co/file/anilistcdn/media/anime/cover/large/bx16498-73IhOXpJZiMF.jpg';
+            return animeItem(
+                context, item, tag, posterUrl, filteredAnimeList, index);
+          },
+        ),
+        desktopBuilder: GridView.builder(
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: getResponsiveCrossAxisCount(MediaQuery.of(context).size.width),
+              mainAxisExtent: 270,
+              crossAxisSpacing: 10),
+          itemCount: filteredAnimeList.length,
+          itemBuilder: (context, index) {
+            final item = filteredAnimeList[index]['media'];
+            final tag = '${Random().nextInt(100000)}$index';
+            final posterUrl = item?['coverImage']?['large'] ??
+                'https://s4.anilist.co/file/anilistcdn/media/anime/cover/large/bx16498-73IhOXpJZiMF.jpg';
+            return animeItemDesktop(
+                context, item, tag, posterUrl, filteredAnimeList, index);
+          },
+        ),
+      ),
+    );
+  }
+
+  GestureDetector animeItem(BuildContext context, item, String tag, posterUrl,
+      List<dynamic> filteredAnimeList, int index) {
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+            context,
+            MaterialPageRoute(
+                builder: (context) => DetailsPage(
+                      id: item['id'],
+                      tag: tag,
+                      posterUrl: posterUrl,
+                    )));
+      },
+      child: Column(
+        children: [
+          Stack(children: [
+            Hero(
+              tag: tag,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(18),
+                child: CachedNetworkImage(
+                  height: 170,
+                  fit: BoxFit.cover,
+                  imageUrl: posterUrl,
+                  placeholder: (context, url) =>
+                      const Center(child: CircularProgressIndicator()),
+                  errorWidget: (context, url, error) => const Icon(Icons.error),
+                ),
+              ),
             ),
-          );
-        },
+            Positioned(
+                bottom: 0,
+                right: 0,
+                child: Container(
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 4, horizontal: 8),
+                  decoration: BoxDecoration(
+                      color: Theme.of(context).colorScheme.surfaceContainer,
+                      borderRadius: const BorderRadius.only(
+                          topLeft: Radius.circular(12),
+                          bottomRight: Radius.circular(16))),
+                  child: Row(
+                    children: [
+                      Icon(
+                        Iconsax.star5,
+                        size: 11,
+                        color: Theme.of(context).colorScheme.primary,
+                      ),
+                      const SizedBox(width: 2),
+                      Text(
+                        (item?['averageScore'] / 10)?.toString() ?? '0.0',
+                        style: TextStyle(
+                            fontSize: 11,
+                            color:
+                                Theme.of(context).colorScheme.inverseSurface ==
+                                        Theme.of(context)
+                                            .colorScheme
+                                            .onPrimaryFixedVariant
+                                    ? Colors.black
+                                    : Theme.of(context)
+                                                .colorScheme
+                                                .onPrimaryFixedVariant ==
+                                            const Color(0xffe2e2e2)
+                                        ? Colors.black
+                                        : Colors.white),
+                      ),
+                    ],
+                  ),
+                )),
+          ]),
+          const SizedBox(height: 7),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                item?['title']?['english'] ?? item?['title']?['romaji'] ?? '?',
+                maxLines: 2,
+                style: const TextStyle(fontSize: 12),
+              ),
+              const SizedBox(height: 2),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    filteredAnimeList[index]?['progress']?.toString() ?? '?',
+                    style: TextStyle(
+                        color:
+                            Theme.of(context).colorScheme.onPrimaryContainer),
+                  ),
+                  Text(' | ',
+                      style: TextStyle(
+                          color: Theme.of(context)
+                              .colorScheme
+                              .inverseSurface
+                              .withOpacity(0.5))),
+                  Text(
+                    item['episodes']?.toString() ?? '?',
+                    style: TextStyle(
+                        color: Theme.of(context)
+                            .colorScheme
+                            .inverseSurface
+                            .withOpacity(0.5)),
+                  ),
+                ],
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+
+  GestureDetector animeItemDesktop(BuildContext context, item, String tag,
+      posterUrl, List<dynamic> filteredAnimeList, int index) {
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+            context,
+            MaterialPageRoute(
+                builder: (context) => DetailsPage(
+                      id: item['id'],
+                      tag: tag,
+                      posterUrl: posterUrl,
+                    )));
+      },
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Stack(children: [
+            SizedBox(
+              height: 200,
+              child: Hero(
+                tag: tag,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(18),
+                  child: CachedNetworkImage(
+                    fit: BoxFit.cover,
+                    imageUrl: posterUrl,
+                    placeholder: (context, url) =>
+                        const Center(child: CircularProgressIndicator()),
+                    errorWidget: (context, url, error) =>
+                        const Icon(Icons.error),
+                    width: double.maxFinite,
+                  ),
+                ),
+              ),
+            ),
+            Positioned(
+                bottom: 0,
+                right: 0,
+                child: Container(
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 4, horizontal: 8),
+                  decoration: BoxDecoration(
+                      color: Theme.of(context).colorScheme.surfaceContainer,
+                      borderRadius: const BorderRadius.only(
+                          topLeft: Radius.circular(12),
+                          bottomRight: Radius.circular(16))),
+                  child: Row(
+                    children: [
+                      Icon(
+                        Iconsax.star5,
+                        size: 11,
+                        color: Theme.of(context).colorScheme.primary,
+                      ),
+                      const SizedBox(width: 2),
+                      Text(
+                        (item?['averageScore'] / 10)?.toString() ?? '0.0',
+                        style: TextStyle(
+                            fontSize: 11,
+                            color:
+                                Theme.of(context).colorScheme.inverseSurface ==
+                                        Theme.of(context)
+                                            .colorScheme
+                                            .onPrimaryFixedVariant
+                                    ? Colors.black
+                                    : Theme.of(context)
+                                                .colorScheme
+                                                .onPrimaryFixedVariant ==
+                                            const Color(0xffe2e2e2)
+                                        ? Colors.black
+                                        : Colors.white),
+                      ),
+                    ],
+                  ),
+                )),
+          ]),
+          const SizedBox(height: 7),
+          Text(
+            item?['title']?['english'] ?? item?['title']?['romaji'] ?? '?',
+            maxLines: 2,
+            style: const TextStyle(fontSize: 12),
+          ),
+          const SizedBox(height: 2),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                filteredAnimeList[index]?['progress']?.toString() ?? '?',
+                style: TextStyle(
+                    color: Theme.of(context).colorScheme.onPrimaryContainer),
+              ),
+              Text(' | ',
+                  style: TextStyle(
+                      color: Theme.of(context)
+                          .colorScheme
+                          .inverseSurface
+                          .withOpacity(0.5))),
+              Text(
+                item['episodes']?.toString() ?? '?',
+                style: TextStyle(
+                    color: Theme.of(context)
+                        .colorScheme
+                        .inverseSurface
+                        .withOpacity(0.5)),
+              ),
+            ],
+          )
+        ],
       ),
     );
   }
