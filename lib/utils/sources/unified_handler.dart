a23cde934fc2c65977cdd865496c4982f31ff61a
Ryan _ <108048963+RyanYuuki@users.noreply.github.com>
2024-12-23 14:34:40 +0530

    chore: removed old extension system

--- lib/utils/sources/unified_handler.dart
+++ lib/utils/sources/unified_handler.dart
@@ -1,22 +1,9 @@
-import 'package:anymex/utils/sources/anime/handler/sources_handler.dart';
-import 'package:anymex/utils/sources/manga/handlers/manga_sources_handler.dart';
 import 'package:anymex/utils/sources/novel/handler/novel_sources_handler.dart';
 import 'package:flutter/material.dart';
 
 class UnifiedSourcesHandler extends ChangeNotifier {
-  SourcesHandler animeHandler = SourcesHandler();
-  MangaSourceHandler mangaSourceHandler = MangaSourceHandler();
   NovelSourcesHandler novelSourcesHandler = NovelSourcesHandler();
-
   UnifiedSourcesHandler();
-
-  SourcesHandler getAnimeInstance() {
-    return animeHandler;
-  }
-
-  MangaSourceHandler getMangaInstance() {
-    return mangaSourceHandler;
-  }
 
   NovelSourcesHandler getNovelInstance() {
     return novelSourcesHandler;
