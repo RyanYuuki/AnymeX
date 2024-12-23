68a7d66bf450574a79a88413fd70a9c9ae7ad857
Ryan _ <108048963+RyanYuuki@users.noreply.github.com>
2024-12-23 14:45:27 +0530

    chore: extensions pages (thanks to @aayush262)

---
+++ lib/pages/Extensions/ExtensionSettings/ListTileChapterFilter.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class ListTileChapterFilter extends StatelessWidget {
+  final String label;
+  final int type;
+  final VoidCallback onTap;
+
+  const ListTileChapterFilter(
+      {super.key,
+      required this.label,
+      required this.type,
+      required this.onTap});
+
+  @override
+  Widget build(BuildContext context) {
+    return CheckboxListTile(
+      dense: true,
+      tristate: true,
+      value: type == 0
+          ? false
+          : type == 1
+              ? true
+              : null,
+      title: Text(
+        label,
+        style: const TextStyle(fontSize: 14),
+      ),
+      controlAffinity: ListTileControlAffinity.leading,
+      onChanged: (value) {
+        onTap.call();
+      },
+    );
+  }
+}
