59b0b36fb94f815d2aa58677eab9248d5376a201
Ryan _ <108048963+RyanYuuki@users.noreply.github.com>
2024-12-23 14:38:21 +0530

    style: changed slider to m3

---
+++ lib/pages/user/settings/modals/tile_with_slider.dart
@@ -0,0 +1,79 @@
+import 'package:anymex/components/android/common/custom_slider.dart';
+import 'package:anymex/components/android/common/custom_tile_ui.dart';
+import 'package:flutter/material.dart';
+
+class TileWithSlider extends StatefulWidget {
+  const TileWithSlider({
+    super.key,
+    required this.sliderValue,
+    required this.onChanged,
+    required this.title,
+    required this.description,
+    required this.icon,
+    required this.min,
+    required this.max,
+    this.divisions,
+    this.iconSize,
+  });
+  final String title;
+  final String description;
+  final double sliderValue;
+  final ValueChanged<double> onChanged;
+  final IconData icon;
+  final double min;
+  final double max;
+  final int? divisions;
+  final double? iconSize;
+
+  @override
+  State<TileWithSlider> createState() => _TileWithSliderState();
+}
+
+class _TileWithSliderState extends State<TileWithSlider> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        CustomTileUi(
+          icon: widget.icon,
+          title: widget.title,
+          description: widget.description,
+          size: widget.iconSize ?? 30,
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20.0),
+          child: Row(
+            children: [
+              SizedBox(
+                width: 25,
+                child: Text(widget.sliderValue.toStringAsFixed(1),
+                    style: TextStyle(
+                        color: Theme.of(context).colorScheme.primary)),
+              ),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: CustomSlider(
+                    enableGlow: true,
+                    value: widget.sliderValue,
+                    onChanged: (newValue) => widget.onChanged(newValue),
+                    min: widget.min,
+                    max: widget.max,
+                    enableComfortPadding: true,
+                    divisions: widget.divisions ?? (widget.max * 10).toInt(),
+                  ),
+                ),
+              ),
+              SizedBox(
+                width: 25,
+                child: Text(widget.max.floor().toString(),
+                    style: TextStyle(
+                        color: Theme.of(context).colorScheme.primary)),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
