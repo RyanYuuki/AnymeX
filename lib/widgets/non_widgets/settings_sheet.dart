df33e4c82c6d5a861008a024b91e4b1f1aeac179
Ryan _ <108048963+RyanYuuki@users.noreply.github.com>
2025-02-01 01:51:33 +0530

    feat: added service selector

--- lib/widgets/non_widgets/settings_sheet.dart
+++ lib/widgets/non_widgets/settings_sheet.dart
@@ -1,15 +1,19 @@
-import 'package:anymex/controllers/services/anilist/anilist_auth.dart';
+import 'package:anymex/controllers/service_handler/service_handler.dart';
 import 'package:anymex/screens/extemsions/ExtensionScreen.dart';
 import 'package:anymex/screens/profile/profile_page.dart';
 import 'package:anymex/screens/settings/settings.dart';
+import 'package:anymex/widgets/minor_widgets/custom_text.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:hugeicons/hugeicons.dart';
+import 'package:iconly/iconly.dart';
 import 'package:iconsax/iconsax.dart';
 
 class SettingsSheet extends StatelessWidget {
   SettingsSheet({super.key});
 
-  final anilistAuth = Get.find<AnilistAuth>();
+  final serviceHandler = Get.find<ServiceHandler>();
 
   static void show(BuildContext context) {
     showModalBottomSheet(
@@ -17,6 +21,82 @@ class SettingsSheet extends StatelessWidget {
       isScrollControlled: true,
       backgroundColor: Theme.of(context).colorScheme.surface,
       builder: (context) => SettingsSheet(),
+    );
+  }
+
+  void showServiceSelector(BuildContext context) {
+    final services = [
+      {
+        'type': ServicesType.anilist,
+        'name': "AniList",
+        'icon':
+            'https://icons.iconarchive.com/icons/simpleicons-team/simple/256/anilist-icon.png',
+      },
+      {
+        'type': ServicesType.mal,
+        'name': "MyAnimeList",
+        'icon':
+            'https://cdn.icon-icons.com/icons2/3913/PNG/512/myanimelist_logo_icon_248409.png',
+      },
+      {
+        'type': ServicesType.simkl,
+        'name': "Simkl",
+        'icon':
+            'https://icon-icons.com/icons2/3915/PNG/512/simkl_logo_icon_249621.png',
+      },
+      if (serviceHandler.extensionService.isExtensionsServiceAllowed.value)
+        {
+          'type': ServicesType.extemsions,
+          'name': "Extensions",
+          'icon': null,
+        },
+    ];
+
+    showModalBottomSheet(
+      context: context,
+      isScrollControlled: true,
+      backgroundColor: Theme.of(context).colorScheme.surface,
+      builder: (context) => Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            const AnymexText(
+              text: "Select Service",
+              size: 16,
+              variant: TextVariant.semiBold,
+            ),
+            ...services.map((service) => ListTile(
+                  leading: service['icon'] != null
+                      ? CachedNetworkImage(
+                          httpHeaders: const {
+                            'Referer': 'https://icon-icons.com/'
+                          },
+                          color: Theme.of(context).colorScheme.primary,
+                          imageUrl: service['icon'] as String,
+                          width: 30,
+                        )
+                      : Icon(
+                          Icons.extension,
+                          size: 30,
+                          color: Theme.of(context).colorScheme.primary,
+                        ),
+                  title: AnymexText(
+                    text: service['name'] as String,
+                    variant: TextVariant.semiBold,
+                    color: serviceHandler.serviceType.value == service['type']
+                        ? Theme.of(context).colorScheme.primary
+                        : null,
+                  ),
+                  onTap: () {
+                    serviceHandler
+                        .changeService(service['type'] as ServicesType);
+                    Navigator.pop(context);
+                  },
+                )),
+          ],
+        ),
+      ),
     );
   }
 
@@ -33,12 +113,15 @@ class SettingsSheet extends StatelessWidget {
             CircleAvatar(
               radius: 24,
               backgroundColor: Theme.of(context).colorScheme.surfaceContainer,
-              child: anilistAuth.isLoggedIn.value
+              child: serviceHandler.isLoggedIn.value
                   ? ClipRRect(
                       borderRadius: BorderRadius.circular(50),
-                      child: Image.network(
+                      child: CachedNetworkImage(
                           fit: BoxFit.cover,
-                          anilistAuth.profileData.value!.avatar!),
+                          errorWidget: (context, url, error) =>
+                              const Icon(IconlyBold.profile),
+                          imageUrl:
+                              serviceHandler.profileData.value.avatar ?? ''),
                     )
                   : Icon(
                       Icons.person,
@@ -49,18 +132,18 @@ class SettingsSheet extends StatelessWidget {
             Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Text(anilistAuth.profileData.value?.name ?? 'Guest'),
+                Text(serviceHandler.profileData.value.name ?? 'Guest'),
                 GestureDetector(
                   onTap: () {
-                    if (anilistAuth.isLoggedIn.value) {
-                      anilistAuth.logout();
+                    if (serviceHandler.isLoggedIn.value) {
+                      serviceHandler.logout();
                     } else {
-                      anilistAuth.login();
+                      serviceHandler.login();
                     }
                     Get.back();
                   },
                   child: Text(
-                    anilistAuth.isLoggedIn.value ? 'Logout' : 'Login',
+                    serviceHandler.isLoggedIn.value ? 'Logout' : 'Login',
                     style: TextStyle(
                         color: Theme.of(context).colorScheme.primary,
                         fontWeight: FontWeight.bold),
@@ -81,7 +164,7 @@ class SettingsSheet extends StatelessWidget {
                 icon: const Icon(Iconsax.notification))
           ]),
           const SizedBox(height: 10),
-          if (anilistAuth.isLoggedIn.value)
+          if (serviceHandler.isLoggedIn.value && serviceHandler.serviceType.value == ServicesType.anilist)
             ListTile(
               leading: const Icon(Iconsax.user),
               title: const Text('View Profile'),
@@ -100,16 +183,18 @@ class SettingsSheet extends StatelessWidget {
               },
             ),
           ListTile(
-            leading: const Icon(Iconsax.toggle_off_circle),
-            title: const Text('Rescue Mode (WIP)'),
+            leading: const Icon(HugeIcons.strokeRoundedAiSetting),
+            title: const Text('Change Service'),
             onTap: () {
               Get.back();
+              showServiceSelector(context);
             },
           ),
           ListTile(
             leading: const Icon(Iconsax.document_download),
             title: const Text('Downloads (WIP)'),
             onTap: () {
+              serviceHandler.simklService.fetchUserMovieList();
               Get.back();
             },
           ),
