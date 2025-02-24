c4642a5a4e38002ecd10b197074fca4efdf5c3db
melral <melvanstalle@hotmail.nl>
2025-02-24 21:24:31 +0100

    Add AirPlay and Chromecast support for iOS in AppDelegate

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -8,6 +8,20 @@ import UIKit
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
+
+    // AirPlay support using AVRoutePickerView (iOS 11+)
+    if #available(iOS 11.0, *) {
+        let routePickerView = AVRoutePickerView(frame: CGRect(x: 20, y: 50, width: 44, height: 44))
+        routePickerView.activeTintColor = UIColor.systemBlue
+        routePickerView.tintColor = UIColor.gray
+        window?.rootViewController?.view.addSubview(routePickerView)
+    }
+
+    // Chromecast support using Google Cast SDK
+    // (Ensure your project is configured with the Google Cast SDK)
+    let castButton = GCKUICastButton(frame: CGRect(x: (window?.rootViewController?.view.frame.width ?? 0) - 64, y: 50, width: 44, height: 44))
+    castButton.tintColor = UIColor.white
+    window?.rootViewController?.view.addSubview(castButton)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
