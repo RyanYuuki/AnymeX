4d128e8a55e8706fc6cbe75cfc3a0ca85a88ce89
Ryan _ <108048963+RyanYuuki@users.noreply.github.com>
2025-03-18 13:12:48 +0530

    fix: rate limit handling fix

--- lib/controllers/services/anilist/anilist_data.dart
+++ lib/controllers/services/anilist/anilist_data.dart
@@ -766,10 +766,10 @@ averageScore
       final media = data['data']['Media'];
       cacheController.addCache(media);
       return Media.fromJson(media);
+    } else if (response.statusCode == 429) {
+      snackBar('Chill for a min, you got rate limited.');
+      throw Exception(response.body);
     } else {
-      if(response.body.contains('Too Many')) {
-        snackBar('Chill for a min, you got rate limited.');
-      }
       throw Exception(response.body);
     }
   }
