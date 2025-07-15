c5f80fb0b08d3f44a04b7050879d66f4fab8b8dd
Itsmechinmoy <167056923+itsmechinmoy@users.noreply.github.com>
2025-07-15 13:47:50 +0530

    Update chocolateyInstall.ps1

--- chocolatey/tools/chocolateyInstall.ps1
+++ chocolatey/tools/chocolateyInstall.ps1
@@ -2,6 +2,6 @@ $ErrorActionPreference = 'Stop';
 
 $packageName = 'com.ryan.anymex'
 $url = 'https://github.com/RyanYuuki/AnymeX/releases/download/v2.9.7/AnymeX-Setup.exe'
-$checksum = 'PUT_CHECKSUM_HERE'
+$checksum = '0e3fabc1ac86d6bc2e1c47863f7da97e595bde75f2240f5953493c25d6de3abb'
 
 Install-ChocolateyPackage $packageName 'exe' '/silent' $url -Checksum $checksum -ChecksumType 'sha256'
