5bae490373871bdf755e886b6baf9809a39b4b5a
Suhas Gudur <suhasgudur2@gmail.com>
2025-07-21 20:19:55 +0530

    Added sauce_finder.dart and sauce_result.dart for reverse image search

---
+++ lib/models/sauce/sauce_result.dart
@@ -0,0 +1,46 @@
+class SauceResult {
+  final String name;
+  final int? episode;
+  final double similarity;
+  final String previewImage;
+  final String videoUrl;
+  final String originalFilename;
+
+  SauceResult({
+    required this.name,
+    required this.episode,
+    required this.similarity,
+    required this.previewImage,
+    required this.videoUrl,
+    required this.originalFilename,
+  });
+
+  factory SauceResult.fromJson(Map<String, dynamic> data) {
+    final filename = data['filename'] ?? 'Unknown';
+    final List<RegExp> regexPatterns = [
+      RegExp(r'\] (.*?) - '),
+      RegExp(r'(?:\[[^\]]*\])\s*\[([^\]]+)\]'),
+      RegExp(r'^(.*?)\s*-')
+    ];
+    String? extractAnimeName(String filename) {
+      for (final regex in regexPatterns) {
+        final match = regex.firstMatch(filename);
+        if (match != null && match.group(1) != null) {
+          return match.group(1)!;
+        }
+      }
+      return null;
+    }
+
+    final animeName = extractAnimeName(filename) ?? filename;
+
+    return SauceResult(
+      name: animeName,
+      episode: data['episode'],
+      similarity: (data['similarity'] as num).toDouble(),
+      previewImage: data['image'],
+      videoUrl: data['video'],
+      originalFilename: filename,
+    );
+  }
+}
