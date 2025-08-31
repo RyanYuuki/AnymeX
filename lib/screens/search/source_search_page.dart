d1f10aaa91f66fc476dd185b92a407324b426d76
Ryan _ <108048963+RyanYuuki@users.noreply.github.com>
2025-09-01 00:33:52 +0530

    feat: source search page

--- lib/screens/search/source_search_page.dart
+++ lib/screens/search/source_search_page.dart
@@ -1,25 +1,21 @@
-import 'package:anymex/controllers/service_handler/params.dart';
 import 'package:anymex/controllers/service_handler/service_handler.dart';
 import 'package:anymex/controllers/source/source_controller.dart';
-import 'package:anymex/models/Media/media.dart';
+import 'package:anymex/utils/extension_utils.dart';
 import 'package:anymex/utils/function.dart';
 import 'package:anymex/widgets/common/glow.dart';
-import 'package:anymex/widgets/common/reusable_carousel.dart';
+import 'package:anymex/widgets/common/future_reusable_carousel.dart';
 import 'package:anymex/widgets/common/search_bar.dart';
-import 'package:anymex/widgets/helper/scroll_wrapper.dart';
 import 'package:dartotsu_extension_bridge/dartotsu_extension_bridge.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class SourceSearchPage extends StatefulWidget {
-  final dynamic source;
   final String? initialTerm;
-  final bool? isManga;
+  final ItemType type;
   const SourceSearchPage({
     super.key,
-    this.source,
     this.initialTerm = "Attack on Titan",
-    this.isManga,
+    this.type = ItemType.anime,
   });
 
   @override
@@ -27,90 +23,73 @@ class SourceSearchPage extends StatefulWidget {
 }
 
 class _SourceSearchPageState extends State<SourceSearchPage> {
-  RxList<Media> searchData = <Media>[].obs;
   final sourceController = Get.find<SourceController>();
   final serviceHandler = Get.find<ServiceHandler>();
   late TextEditingController textController;
-  late bool isManga;
-  late bool wasAllSelected;
-  RxList<ReusableCarousel> searchCarousels = <ReusableCarousel>[].obs;
+  RxString currentSearchTerm = ''.obs;
 
   @override
   void initState() {
     super.initState();
-    textController = TextEditingController(text: widget.initialTerm);
-    determineItemType();
-    _search();
-  }
-
-  void determineItemType() {
-    if (widget.source is List<Source>) {
-      isManga = widget.source[0].isManga ?? widget.isManga ?? false;
-      wasAllSelected = true;
-    } else if (widget.source is Source) {
-      isManga = widget.source?.isManga ?? widget.isManga ?? false;
-      wasAllSelected = false;
-    } else {
-      isManga = widget.isManga ?? false;
-      wasAllSelected = false;
+    textController = TextEditingController(text: widget.initialTerm ?? '');
+    if (widget.initialTerm?.isNotEmpty == true) {
+      _search();
     }
   }
 
   Future<void> _search() async {
-    if (wasAllSelected) {
-      searchCarousels.clear();
-      for (Source e in widget.source) {
-        final data = (await e.methods.search(textController.text, 1, [])).list;
-        if (data.isNotEmpty) {
-          searchCarousels.add(
-            ReusableCarousel(
-              data: data,
-              title: e.name!,
-              variant: DataVariant.extension,
-              source: e,
-            ),
-          );
-        }
-      }
-    } else {
-      final data =
-          await serviceHandler.search(SearchParams(query: textController.text));
-      searchData.value = data ?? [];
-    }
+    if (textController.text.trim().isEmpty) return;
+
+    final searchTerm = textController.text.trim();
+    currentSearchTerm.value = searchTerm;
   }
 
   @override
   Widget build(BuildContext context) {
     return Glow(
       child: Scaffold(
-        body: ScrollWrapper(
-          children: [
-            Row(
+        body: SafeArea(
+          child: SingleChildScrollView(
+            child: Column(
               children: [
-                const SizedBox(width: 10),
-                IconButton(
-                    onPressed: () => Get.back(),
-                    icon: const Icon(Icons.arrow_back_ios_new)),
-                Expanded(
-                  child: CustomSearchBar(
-                    onSubmitted: (v) => _search(),
-                    controller: textController,
-                    hintText: isManga ? "Search Manga..." : "Search Anime...",
-                  ),
+                Row(
+                  children: [
+                    const SizedBox(width: 10),
+                    IconButton(
+                      onPressed: () => Get.back(),
+                      icon: const Icon(Icons.arrow_back_ios_new),
+                    ),
+                    Expanded(
+                      child: CustomSearchBar(
+                        onSubmitted: (v) => _search(),
+                        controller: textController,
+                        disableIcons: true,
+                        hintText:
+                            "Search ${widget.type.name.capitalizeFirst}...",
+                      ),
+                    ),
+                  ],
                 ),
+                ...widget.type.extensions
+                    .map((extension) => FutureReusableCarousel(
+                          title: extension.name ?? '',
+                          future: extension.methods.search(
+                              textController.text, 1, []).then((e) => e.list),
+                          type: widget.type,
+                          variant: DataVariant.extension,
+                          source: extension,
+                        )),
               ],
             ),
-            Obx(() => wasAllSelected
-                ? Column(
-                    children: searchCarousels.value,
-                  )
-                : ReusableCarousel(
-                    data: searchData.value,
-                    title: widget.source.name,
-                  ))
-          ],
+          ),
         ),
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    textController.dispose();
+    super.dispose();
   }
 }
