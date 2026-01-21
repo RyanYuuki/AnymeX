8c7e7a2547a717267533010cf9f86d608b04c430
Rieversed <LunarScytheYT@gmail.com>
2026-01-21 11:30:37 +0100

    Refractor

--- lib/screens/manga/widgets/reader/reader_view.dart
+++ lib/screens/manga/widgets/reader/reader_view.dart
@@ -7,7 +7,7 @@ import 'package:anymex/screens/manga/controller/reader_controller.dart';
 import 'package:anymex/widgets/custom_widgets/anymex_progress.dart';
 import 'package:dartotsu_extension_bridge/dartotsu_extension_bridge.dart';
 import 'package:extended_image/extended_image.dart';
-import 'package:anymex/screens/manga/utils/image_cropper.dart';
+import 'package:anymex/utils/image_cropper.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
