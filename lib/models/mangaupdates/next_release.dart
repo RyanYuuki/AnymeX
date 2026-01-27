5a818d738fb11088b00cb842b48c07bbdb7396f6
Itsmechinmoy <167056923+itsmechinmoy@users.noreply.github.com>
2026-01-27 14:19:16 +0530

    Update next_release.dart

--- lib/models/mangaupdates/next_release.dart
+++ lib/models/mangaupdates/next_release.dart
@@ -1,7 +1,7 @@
 class NextRelease {
   final DateTime? nextReleaseDate;
   final int? averageIntervalDays;
-  final String? nextChapter; // Added this field
+  final String? nextChapter;
   final String? error;
 
   NextRelease({
@@ -14,6 +14,6 @@ class NextRelease {
   @override
   String toString() {
     if (error != null) return 'Error: $error';
-    return 'Next: $nextReleaseDate (Interval: $averageIntervalDays days)';
+    return 'Next: $nextChapter on $nextReleaseDate (Interval: $averageIntervalDays days)';
   }
 }
