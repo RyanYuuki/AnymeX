4d89df0f88514245f5328da4db212ef1800780de
Ryan _ <rehank220358@gmail.com>
2026-02-09 23:33:41 +0530

    feat: added support for betterplayer, fixed tracking when denied.

--- lib/screens/local_source/player/offline_player.dart
+++ lib/screens/local_source/player/offline_player.dart
@@ -4,17 +4,15 @@ import 'package:anymex/models/Offline/Hive/episode.dart';
 import 'package:anymex/screens/anime/watch/controller/player_controller.dart';
 import 'package:anymex/screens/anime/watch/controls/bottom_controls.dart';
 import 'package:anymex/screens/anime/watch/controls/center_controls.dart';
-import 'package:anymex/screens/anime/watch/controls/widgets/double_tap_seek.dart';
-import 'package:anymex/screens/anime/watch/controls/widgets/overlay.dart';
 import 'package:anymex/screens/anime/watch/controls/top_controls.dart';
+import 'package:anymex/screens/anime/watch/controls/widgets/double_tap_seek.dart';
 import 'package:anymex/screens/anime/watch/controls/widgets/episodes_pane.dart';
+import 'package:anymex/screens/anime/watch/controls/widgets/overlay.dart';
 import 'package:anymex/screens/anime/watch/controls/widgets/subtitle_text.dart';
 import 'package:anymex/screens/anime/watch/subtitles/subtitle_view.dart';
 import 'package:anymex/screens/anime/widgets/media_indicator.dart';
 import 'package:flutter/material.dart';
-import 'package:anymex/utils/theme_extensions.dart';
 import 'package:get/get.dart';
-import 'package:media_kit_video/media_kit_video.dart';
 
 class LocalEpisode {
   final String folderName;
@@ -69,13 +67,14 @@ class _OfflineWatchPageState extends State<OfflineWatchPage> {
         body: Stack(
       children: [
         Obx(() {
-          return Video(
-              controller: controller.playerController,
-              fit: controller.videoFit.value,
-              resumeUponEnteringForegroundMode: true,
-              subtitleViewConfiguration:
-                  const SubtitleViewConfiguration(visible: false),
-              controls: (state) => const SizedBox.shrink());
+          return controller.videoWidget;
+          // return Video(
+          //     controller: controller.playerController,
+          //     fit: controller.videoFit.value,
+          //     resumeUponEnteringForegroundMode: true,
+          //     subtitleViewConfiguration:
+          //         const SubtitleViewConfiguration(visible: false),
+          //     controls: (state) => const SizedBox.shrink());
         }),
         PlayerOverlay(controller: controller),
         SubtitleText(controller: controller),
