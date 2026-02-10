5552cc5408326569a1d27f78378ffb12e816387e
Itsmechinmoy <167056923+itsmechinmoy@users.noreply.github.com>
2026-02-10 18:54:19 +0530

    Update news_item.dart

--- lib/models/mangaupdates/news_item.dart
+++ lib/models/mangaupdates/news_item.dart
@@ -8,9 +8,9 @@ class NewsItem {
   factory NewsItem.fromMangaBaka(Map<String, dynamic> json) {
     return NewsItem(
       title: json['title'] ?? '',
-      url: json['link'] ?? '',
-      date: json['time'] != null 
-          ? DateTime.fromMillisecondsSinceEpoch(json['time'] * 1000) 
+      url: json['url'] ?? '',
+      date: json['published_at'] != null 
+          ? DateTime.tryParse(json['published_at']) 
           : null,
     );
   }
