43a38fcc633f8021164c5420447c5cd906dde724
Sheby <83452219+Shebyyy@users.noreply.github.com>
2026-02-17 21:57:16 +0530

    Add multiple reader control themes to registry

--- lib/screens/manga/widgets/reader/themes/setup/reader_control_theme_registry.dart
+++ lib/screens/manga/widgets/reader/themes/setup/reader_control_theme_registry.dart
@@ -1,5 +1,13 @@
 import 'package:anymex/screens/manga/widgets/reader/themes/reader_control_themes/default_reader_control_theme.dart';
 import 'package:anymex/screens/manga/widgets/reader/themes/reader_control_themes/ios_reader_control_theme.dart';
+import 'package:anymex/screens/manga/widgets/reader/themes/reader_control_themes/modern_reader_control_theme.dart';
+import 'package:anymex/screens/manga/widgets/reader/themes/reader_control_themes/minimal_reader_control_theme.dart';
+import 'package:anymex/screens/manga/widgets/reader/themes/reader_control_themes/retro_reader_control_theme.dart';
+import 'package:anymex/screens/manga/widgets/reader/themes/reader_control_themes/cyberpunk_reader_control_theme.dart';
+import 'package:anymex/screens/manga/widgets/reader/themes/reader_control_themes/glass_reader_control_theme.dart';
+import 'package:anymex/screens/manga/widgets/reader/themes/reader_control_themes/cinema_reader_control_theme.dart';
+import 'package:anymex/screens/manga/widgets/reader/themes/reader_control_themes/gaming_reader_control_theme.dart';
+import 'package:anymex/screens/manga/widgets/reader/themes/reader_control_themes/anime_reader_control_theme.dart';
 import 'package:anymex/screens/manga/widgets/reader/themes/setup/reader_control_theme.dart';
 
 class ReaderControlThemeRegistry {
@@ -8,6 +16,14 @@ class ReaderControlThemeRegistry {
   static List<ReaderControlTheme> themes = [
     DefaultReaderControlTheme(),
     IOSReaderControlTheme(),
+    ModernReaderControlTheme(),
+    MinimalReaderControlTheme(),
+    RetroReaderControlTheme(),
+    CyberpunkReaderControlTheme(),
+    GlassReaderControlTheme(),
+    CinemaReaderControlTheme(),
+    GamingReaderControlTheme(),
+    AnimeReaderControlTheme(),
   ];
 
   static ReaderControlTheme resolve(String id) {
