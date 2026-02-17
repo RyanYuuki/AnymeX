65e29040b2350e8fa8e9db483cee5defe54b51fb
Sheby <83452219+Shebyyy@users.noreply.github.com>
2026-02-17 21:25:40 +0530

    Updated

--- lib/screens/anime/watch/controls/themes/setup/player_control_theme_registry.dart
+++ lib/screens/anime/watch/controls/themes/setup/player_control_theme_registry.dart
@@ -1,7 +1,13 @@
+import 'package:anymex/screens/anime/watch/controls/themes/player_control_themes/cyberpunk_player_control_theme.dart';
 import 'package:anymex/screens/anime/watch/controls/themes/player_control_themes/default_player_control_theme.dart';
+import 'package:anymex/screens/anime/watch/controls/themes/player_control_themes/floating_orbs_player_control_theme.dart';
 import 'package:anymex/screens/anime/watch/controls/themes/player_control_themes/ios26_player_control_theme.dart';
+import 'package:anymex/screens/anime/watch/controls/themes/player_control_themes/minimal_player_control_theme.dart';
 import 'package:anymex/screens/anime/watch/controls/themes/player_control_themes/netflix_desktop_player_theme.dart.dart';
 import 'package:anymex/screens/anime/watch/controls/themes/player_control_themes/netflix_mobile_player_theme.dart';
+import 'package:anymex/screens/anime/watch/controls/themes/player_control_themes/prime_video_player_control_theme.dart';
+import 'package:anymex/screens/anime/watch/controls/themes/player_control_themes/retro_vhs_player_control_theme.dart';
+import 'package:anymex/screens/anime/watch/controls/themes/player_control_themes/youtube_player_control_theme.dart';
 import 'package:anymex/screens/anime/watch/controls/themes/setup/player_control_theme.dart';
 
 class PlayerControlThemeRegistry {
@@ -11,7 +17,13 @@ class PlayerControlThemeRegistry {
     DefaultPlayerControlTheme(),
     Ios26PlayerControlTheme(),
     NetflixDesktopPlayerControlTheme(),
-    NetflixMobilePlayerControlTheme()
+    NetflixMobilePlayerControlTheme(),
+    PrimeVideoPlayerControlTheme(),
+    YouTubePlayerControlTheme(),
+    MinimalPlayerControlTheme(),
+    CyberpunkPlayerControlTheme(),
+    FloatingOrbsPlayerControlTheme(),
+    RetroVhsPlayerControlTheme(),
   ];
 
   static PlayerControlTheme resolve(String id) {
