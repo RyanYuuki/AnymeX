f78ea7ed734db29012af01ee298ad63057ff6366
Itsmechinmoy <167056923+itsmechinmoy@users.noreply.github.com>
2026-03-06 14:41:17 +0530

    Removed duplicate code from simkl calendar

--- lib/controllers/services/simkl/calendar_data.dart
+++ lib/controllers/services/simkl/calendar_data.dart
@@ -21,58 +21,6 @@ Future<void> fetchSimklCalendarData(RxList<Media> callbackData, {bool isMovies =
     List<Media> newMediaList = schedules
         .map<Media?>((schedule) {
           final media = Media.fromSmallSimkl(schedule, !isMovies);
-          
-          DateTime airDate;
-          try {
-            airDate = DateTime.parse(schedule['release_date'] ?? schedule['date'] ?? '');
-          } catch (e) {
-            return null;
-          }
-          
-          media.nextAiringEpisode = NextAiringEpisode(
-            airingAt: airDate.millisecondsSinceEpoch ~/ 1000,
-            timeUntilAiring: airDate.difference(DateTime.now()).inSeconds,
-            episode: 1,
-          );
-          
-          media.aired = schedule['release_date'] ?? schedule['date'] ?? '';
-          
-          return media;
-        })
-        .toList()
-        .whereType<Media>()
-        .toList();
-
-    callbackData.addAll(newMediaList);
-
-    Logger.i('Fetched ${callbackData.length} total Simkl calendar items so far.');
-  } else {
-    Logger.i('Error: ${response.body}');
-    throw Exception('Failed to load Simkl calendar data: ${response.statusCode}');
-  }
-}import 'dart:convert';
-
-import 'package:anymex/controllers/service_handler/service_handler.dart';
-import 'package:anymex/models/Media/media.dart';
-import 'package:anymex/utils/logger.dart';
-import 'package:get/get.dart';
-import 'package:http/http.dart' as http;
-import 'package:flutter_dotenv/flutter_dotenv.dart';
-
-Future<void> fetchSimklCalendarData(RxList<Media> callbackData, {bool isMovies = false}) async {
-  final String url = isMovies 
-      ? 'https://data.simkl.in/calendar/movie_release.json'
-      : 'https://data.simkl.in/calendar/tv.json';
-
-  final response = await http.get(Uri.parse(url));
-
-  if (response.statusCode == 200) {
-    final List<dynamic> schedules = json.decode(response.body);
-    final isMAL = serviceHandler.serviceType.value == ServicesType.mal;
-
-    List<Media> newMediaList = schedules
-        .map<Media?>((schedule) {
-          final media = Media.fromSmallSimkl(schedule, !isMovies);
           return media;
         })
         .toList()
