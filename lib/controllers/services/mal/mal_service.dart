4de038098288ea28b94a77e7e946d89686074a20
Rieversed <LunarScytheYT@gmail.com>
2026-03-08 19:34:24 +0100

    press and hold to open media

--- lib/controllers/services/mal/mal_service.dart
+++ lib/controllers/services/mal/mal_service.dart
@@ -15,9 +15,11 @@ import 'package:anymex/models/Anilist/anilist_profile.dart';
 import 'package:anymex/models/Media/media.dart';
 import 'package:anymex/models/Service/base_service.dart';
 import 'package:anymex/models/Service/online_service.dart';
+import 'package:anymex/screens/anime/details_page.dart';
 import 'package:anymex/screens/home_page.dart';
 import 'package:anymex/screens/library/online/anime_list.dart';
 import 'package:anymex/screens/library/online/manga_list.dart';
+import 'package:anymex/screens/manga/details_page.dart';
 import 'package:anymex/screens/other_features.dart';
 import 'package:anymex/utils/fallback/fallback_manga.dart';
 import 'package:anymex/utils/function.dart';
@@ -34,6 +36,37 @@ import 'package:get/get.dart';
 import 'package:http/http.dart' as http;
 
 class MalService extends GetxController implements BaseService, OnlineService {
+  Media? _firstMediaWithCover(Iterable<Media> mediaList) {
+    for (final media in mediaList) {
+      final cover = media.cover;
+      if (cover != null && cover.isNotEmpty) {
+        return media;
+      }
+    }
+    return null;
+  }
+
+  Media? _lastMediaWithCover(Iterable<Media> mediaList) {
+    final list = mediaList.toList(growable: false);
+    for (var index = list.length - 1; index >= 0; index--) {
+      final media = list[index];
+      final cover = media.cover;
+      if (cover != null && cover.isNotEmpty) {
+        return media;
+      }
+    }
+    return null;
+  }
+
+  void _openHomeButtonMedia(Media media) {
+    final tag = 'home-button-${media.serviceType.name}-${media.id}';
+    if (media.mediaType == ItemType.manga) {
+      navigate(() => MangaDetailsPage(media: media, tag: tag));
+      return;
+    }
+    navigate(() => AnimeDetailsPage(media: media, tag: tag));
+  }
+
   @override
   RxList<TrackedMedia> animeList = <TrackedMedia>[].obs;
   @override
@@ -205,6 +238,16 @@ class MalService extends GetxController implements BaseService, OnlineService {
           final overflow = constraints.maxWidth < 900;
           final overflowSecond =
               !isDesktop ? false : constraints.maxWidth < 600;
+          final animeButtonMedia = _firstMediaWithCover(trendingAnimes);
+          final mangaButtonMedia = _firstMediaWithCover(
+            trendingManga.isNotEmpty ? trendingManga : trendingMangas,
+          );
+          final otherButtonMedia = _lastMediaWithCover([
+            ...popularAnimes,
+            ...(topManga.isNotEmpty ? topManga : popularMangas),
+            ...(trendingManga.isNotEmpty ? trendingManga : trendingMangas),
+            ...trendingAnimes,
+          ]);
           return Wrap(
             alignment: WrapAlignment.center,
             spacing: 15,
@@ -213,14 +256,14 @@ class MalService extends GetxController implements BaseService, OnlineService {
                 width: width,
                 height: !isDesktop ? 70 : 90,
                 buttonText: "ANIME LIST",
-                backgroundImage: trendingAnimes.isEmpty
-                    ? ''
-                    : trendingAnimes.firstWhere((e) => e.cover != null).cover ??
-                        '',
+                backgroundImage: animeButtonMedia?.cover ?? '',
                 borderRadius: 16.multiplyRadius(),
                 onPressed: () {
                   navigate(() => const AnimeList());
                 },
+                onLongPress: animeButtonMedia == null
+                    ? null
+                    : () => _openHomeButtonMedia(animeButtonMedia),
               ),
               Padding(
                 padding: EdgeInsets.only(top: overflowSecond ? 8.0 : 0),
@@ -229,12 +272,13 @@ class MalService extends GetxController implements BaseService, OnlineService {
                   height: !isDesktop ? 70 : 90,
                   buttonText: "MANGA LIST",
                   borderRadius: 16.multiplyRadius(),
-                  backgroundImage:
-                      trendingMangas.firstWhere((e) => e.cover != null).cover ??
-                          '',
+                  backgroundImage: mangaButtonMedia?.cover ?? '',
                   onPressed: () {
                     navigate(() => const AnilistMangaList());
                   },
+                  onLongPress: mangaButtonMedia == null
+                      ? null
+                      : () => _openHomeButtonMedia(mangaButtonMedia),
                 ),
               ),
               Padding(
@@ -246,16 +290,13 @@ class MalService extends GetxController implements BaseService, OnlineService {
                   height: !isDesktop ? 70 : 90,
                   buttonText: "OTHER",
                   borderRadius: 16.multiplyRadius(),
-                  backgroundImage: [
-                        ...popularAnimes,
-                        ...popularMangas,
-                        ...trendingMangas,
-                        ...trendingAnimes
-                      ].where((e) => e.cover != null).last.cover ??
-                      '',
+                  backgroundImage: otherButtonMedia?.cover ?? '',
                   onPressed: () {
                     navigate(() => const OtherFeaturesPage());
                   },
+                  onLongPress: otherButtonMedia == null
+                      ? null
+                      : () => _openHomeButtonMedia(otherButtonMedia),
                 ),
               ),
             ],
