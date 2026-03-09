1e25c83d6761e7ee9717399bb49ea229eec487d0
Sap <ibbrahimsafi0@gmail.com>
2026-03-08 23:25:07 -0400

    Update details_page.dart

--- lib/screens/anime/details_page.dart
+++ lib/screens/anime/details_page.dart
@@ -865,7 +865,7 @@ class _AnimeDetailsPageState extends State<AnimeDetailsPage> {
           animeProgress: animeProgress,
           currentAnime: currentAnime,
           media: anilistData ?? widget.media,
-          onUpdate: (id, score, status, progress) async {
+          onUpdate: (id, score, status, progress, startedAt, completedAt) async {
             final fetcher = widget.media.serviceType;
             final id = fetcher.onlineService.currentMedia.value.id;
             fetcher.onlineService.updateListEntry(UpdateListEntryParams(
@@ -874,10 +874,14 @@ class _AnimeDetailsPageState extends State<AnimeDetailsPage> {
                 isAnime: true,
                 score: score,
                 status: status,
-                progress: progress));
+                progress: progress,
+                startedAt: startedAt,
+                completedAt: completedAt));
             currentAnime.value?.score = score.toString();
             currentAnime.value?.watchingStatus = status;
             currentAnime.value?.episodeCount = progress.toString();
+            currentAnime.value?.startedAt = startedAt;
+            currentAnime.value?.completedAt = completedAt;
             setState(() {});
           },
           onDelete: (s) async {
