b28245da8ff9fb6f4944914b3050150f9502acb0
Itsmechinmoy <167056923+itsmechinmoy@users.noreply.github.com>
2026-03-09 11:24:31 +0530

    Update player_adaptor.dart

--- lib/models/player/player_adaptor.dart
+++ lib/models/player/player_adaptor.dart
@@ -19,6 +19,7 @@ class PlayerSettings {
   bool autoSkipOP;
   bool autoSkipED;
   bool autoSkipOnce;
+  bool autoSkipRecap;
   bool enableSwipeControls;
   int markAsCompleted;
   bool transitionSubtitle;
@@ -49,6 +50,7 @@ class PlayerSettings {
     this.autoSkipED = false,
     this.autoSkipOP = false,
     this.autoSkipOnce = false,
+    this.autoSkipRecap = false,
     this.enableSwipeControls = true,
     this.markAsCompleted = 90,
     this.autoTranslate = false,
@@ -104,6 +106,8 @@ class PlayerSettings {
       autoSkipED: PlayerSettingsKeys.autoSkipED.get<bool>(defaults.autoSkipED),
       autoSkipOnce:
           PlayerSettingsKeys.autoSkipOnce.get<bool>(defaults.autoSkipOnce),
+      autoSkipRecap:
+          PlayerSettingsKeys.autoSkipRecap.get<bool>(defaults.autoSkipRecap),
       enableSwipeControls: PlayerSettingsKeys.enableSwipeControls
           .get<bool>(defaults.enableSwipeControls),
       markAsCompleted:
