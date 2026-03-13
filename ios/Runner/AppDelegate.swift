eb7ab8b84c6b79deb417b61500be9f9301f2f132
NITHINSPACETIME <166204452+NITHINSPACETIME@users.noreply.github.com>
2026-03-13 16:45:57 +0530

    fix: ios pip

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -15,16 +15,4 @@ import UIKit
   override func registerPlugin(_ registry: FlutterPluginRegistry) {
     GeneratedPluginRegistrant.register(with: registry)
   }
-}import Flutter
-import UIKit
-
-@UIApplicationMain
-@objc class AppDelegate: FlutterAppDelegate {
-  override func application(
-    _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-  ) -> Bool {
-    GeneratedPluginRegistrant.register(with: self)
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
 }
