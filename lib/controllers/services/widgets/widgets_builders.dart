0ab857ddb959af3c73a9dbdecc17b1521601cde1
Sheby <83452219+Shebyyy@users.noreply.github.com>
2026-03-17 18:48:27 +0530

    fix: missing imports

--- lib/controllers/services/widgets/widgets_builders.dart
+++ lib/controllers/services/widgets/widgets_builders.dart
@@ -13,6 +13,8 @@ import 'package:anymex/widgets/common/cards/card_gate.dart';
 import 'package:anymex/widgets/common/future_reusable_carousel.dart';
 import 'package:anymex/widgets/common/reusable_carousel.dart';
 import 'package:anymex/widgets/custom_widgets/custom_text.dart';
+import 'package:anymex/widgets/helper/platform_builder.dart';
+import 'package:anymex/widgets/media_items/media_peek_popup.dart';
 import 'package:anymex_extension_runtime_bridge/Models/Source.dart';
 import 'package:anymex_extension_runtime_bridge/anymex_extension_runtime_bridge.dart';
 import 'package:flutter/material.dart';
