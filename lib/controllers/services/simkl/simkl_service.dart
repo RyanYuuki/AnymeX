dc01805c75acb65a3fe3b7eb5b2770465cf1f087
Sheby <83452219+Shebyyy@users.noreply.github.com>
2026-04-09 10:45:36 +0530

    fix: resolve ItemType undefined error in simkl_service

--- lib/controllers/services/simkl/simkl_service.dart
+++ lib/controllers/services/simkl/simkl_service.dart
@@ -9,6 +9,7 @@ import 'package:anymex/controllers/service_handler/service_handler.dart';
 import 'package:anymex/controllers/services/underrated_service.dart';
 import 'package:anymex/controllers/services/widgets/widgets_builders.dart';
 import 'package:anymex/screens/community/community_recommendations_page.dart';
+import 'package:anymex_extension_runtime_bridge/anymex_extension_runtime_bridge.dart';
 import 'package:anymex/controllers/settings/methods.dart';
 import 'package:anymex/database/data_keys/keys.dart';
 import 'package:anymex/models/Anilist/anilist_media_user.dart';
