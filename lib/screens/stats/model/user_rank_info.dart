97d7d05494837d5c1f177a80032d48eb85b0f4ea
Ryan _ <rehank220358@gmail.com>
2026-08-25 23:17:01 +0530

    feat: user xp system

---
+++ lib/screens/stats/model/user_rank_info.dart
@@ -0,0 +1,17 @@
+class UserRankInfo {
+  final String title;
+  final double hours;
+  final int nextMaxHours;
+  final double progress;
+  final String rankIcon;
+  final int points;
+
+  UserRankInfo({
+    required this.title,
+    required this.hours,
+    required this.nextMaxHours,
+    required this.progress,
+    required this.rankIcon,
+    required this.points,
+  });
+}
